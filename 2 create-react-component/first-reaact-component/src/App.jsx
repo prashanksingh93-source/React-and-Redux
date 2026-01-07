7684690221f6aa378e0dfb32825ccbe7e8f309aa
prashanksingh93-source <prashanksingh93@gmail.com>
2026-01-07 17:40:04 +0530

    fix

---
+++ 2 create-react-component/first-reaact-component/src/App.jsx
@@ -0,0 +1,12 @@
+import Hello from"./Name";
+
+function App() {
+  return <div>
+
+    <Hello></Hello>
+  </div>
+
+  
+}
+
+export default App
---
+++ 2 create-react-component/first-reaact-component/src/Name.jsx
@@ -0,0 +1,7 @@
+function Hello(){
+    let myName='Annu';
+    let age="30=";
+    function Name(){return 'prachank'};
+    return <p>display{myName} name diaplay{myName()}</p>
+}
+export default Hello;
