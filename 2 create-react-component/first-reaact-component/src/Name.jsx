50a3db98f167a3a59de586b296e9d94f4ff7aa39
prashanksingh93-source <prashanksingh93@gmail.com>
2026-01-08 13:25:57 +0530

    dlt

--- 2 create-react-component/first-reaact-component/src/Name.jsx
+++
@@ -1,7 +0,0 @@
-function Hello(){
-    let myName='Annu';
-    let age="30=";
-    function Name(){return 'prachank'};
-    return <p>display{myName} name diaplay{myName()}</p>
-}
-export default Hello;
