575eda83944c07bfdf290069ad7d2a2837804ea8
prashanksingh93-source <prashanksingh93@gmail.com>
2026-01-11 11:39:18 +0530

    fragment

---
+++ 03Fragment/learning-fragment/src/App.jsx
@@ -0,0 +1,22 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import React from 'react'
+import './App.css'
+import"bootstrap/dist/css/bootstrap.min.css"
+
+function App() {
+  return(<React.Fragment>
+  <div><h1>Name</h1></div>
+    <div><ul class="list-group">
+  <li class="list-group-item">An item</li>
+  <li class="list-group-item">A second item</li>
+  <li class="list-group-item">A third item</li>
+  <li class="list-group-item">A fourth item</li>
+  <li class="list-group-item">And a fifth one</li>
+</ul></div>
+ </React.Fragment>
+  )
+}
+
+export default App
